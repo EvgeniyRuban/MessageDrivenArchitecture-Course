eac44c1b12da994c43ba2adaa0b5e2d93e2d5855
EvgeniyRuban <eugene_1997@inbox.ru>
2022-09-26 01:57:37 +0600

    Added unbooking operations.

---
+++ RestaurantApp/Models/Table.cs
@@ -0,0 +1,18 @@
+﻿namespace RestaurantApp;
+
+public sealed class Table
+{
+    public Table(int id, int seatsCount)
+    {
+        Id = id;
+        State = TableState.Free;
+        SeatsCount = seatsCount;
+    }
+
+    public int Id { get; }
+    public TableState State { get; private set; }
+    public int SeatsCount { get; }
+
+    public void SetState(TableState state) => State = state;
+
+}
