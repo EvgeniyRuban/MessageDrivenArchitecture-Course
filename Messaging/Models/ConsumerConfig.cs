a0978eb8eb0117f19b6edbd1fbd28f9989647cc1
EvgeniyRuban <eugene_1997@inbox.ru>
2022-09-29 04:12:14 +0600

    RabbitMQ injected.

---
+++ Messaging/Models/ConsumerConfig.cs
@@ -0,0 +1,13 @@
+﻿namespace Messaging;
+
+public sealed class ConsumerConfig
+{
+    public string HostName { get; set; } = null!;
+    public string VirtualHost { get; set; } = null!;
+    public string UserName { get; set; } = null!;
+    public string Password { get; set; } = null!;
+    public int Port { get; set; }
+    public string QueueName { get; set; } = null!;
+    public string RoutingKey { get; set; } = null!;
+    public string ExchangeName { get; set; } = null!;
+}
---
+++ Restaurant.Notification/Startup.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using Messaging;
+using Microsoft.Extensions.Hosting;
+
+namespace Restaurant.Notification;
+
+public class Startup : BackgroundService
+{
+    private readonly Consumer _consumer;
+
+    public Startup()
+    {
+        _consumer = new (new ConsumerConfig
+        {
+            UserName = "sfbzerjl",
+            Password = "7sdVW8O46lm2XW98UUt1-V3Ia2VjMkry",
+            HostName = "shrimp-01.rmq.cloudamqp.com",
+            VirtualHost = "sfbzerjl",
+            QueueName = "BookingNotification",
+            RoutingKey = "BookingNotification",
+            ExchangeName = "direct_exchange",
+            Port = 5672,
+        });
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        _consumer.Receive((sender, args) =>
+        {
+            var body = args.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            Console.WriteLine($" Received [x]: {message}");
+        });
+    }
+}
